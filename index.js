9c699707fc913f973876cd3cfa87484cc4af2715
cooldued5279 <157918606+cooldued5279@users.noreply.github.com>
2024-03-10 15:42:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Welcome to Emirates!","مرحبا بكم في طيران الإمارات"];
 
 
 let currentIndex = 0;
